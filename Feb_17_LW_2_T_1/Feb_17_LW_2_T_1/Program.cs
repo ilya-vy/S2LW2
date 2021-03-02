2a4ec055c84a89809b3ffd33f09477ece53bc0b1
ilya-vy <70759382+ilya-vy@users.noreply.github.com>
2021-03-02 07:15:04 -0800

    1

---
+++ Feb_17_LW_2_T_1/Feb_17_LW_2_T_1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Feb_17_LW_2_T_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> numbers = new MyList<int>();
+
+            numbers.Add(6);
+            numbers.Add(88);
+            numbers.Add(8234);
+            numbers.Add(734);
+
+            for (int i = 0; i < numbers.ListLetgth; i++)
+            {
+                Console.WriteLine(numbers.GetElement(i));
+            }
+            Console.WriteLine(numbers.ListLetgth);
+            Console.ReadKey();
+        }
+    }
+    class MyList<T>
+    {
+        private T[] Array { get; set; }
+        public int ListLetgth
+        {
+            get
+            {
+                return listLetgth;
+            }
+        }
+        private int listLetgth;
+        public int ArraySize = 1;
+        public void Add(T item)
+        {
+            if (ListLetgth == 0)
+            {
+                Array = new T[ArraySize];
+                Array[ListLetgth] = item;
+                listLetgth++;
+                ArraySize++;
+            }
+            else
+            {
+                T[] Darr = Array;
+                Array = new T[ArraySize];
+                for (int i = 0; i < Darr.Length; i++)
+                {
+                    Array[i] = Darr[i];
+                }
+                Array[ListLetgth] = item;
+                listLetgth++;
+                ArraySize++;
+            }
+           
+        }
+        public T GetElement(int index)
+        {
+            return Array[index];
+        }
+    }
+}
---
+++ Feb_17_LW_2_T_2/Feb_17_LW_2_T_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace Feb_17_LW_2_T_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int, string> dictionary = new MyList<int, string>();
+
+            dictionary.Add(88, "String2");
+            dictionary.Add(734, "String03FD");
+
+            Console.WriteLine(dictionary.ListLetgth);
+            Console.WriteLine(dictionary.GetElement(734));
+
+            Console.ReadKey();
+        }
+    }
+    class MyList<T,R>
+    {
+        private T[] KeyArray { get; set; }
+        private R[] ValueArray { get; set; }
+
+        public int ListLetgth
+        {
+            get
+            {
+                return listLetgth;
+            }
+        }
+        private int listLetgth;
+        public int ArraySize = 1;
+        public void Add(T key, R value)
+        {
+            if (ListLetgth == 0)
+            {
+                KeyArray = new T[ArraySize];
+                KeyArray[ListLetgth] = key;
+
+                ValueArray = new R[ArraySize];
+                ValueArray[ListLetgth] = value;
+
+                listLetgth++;
+                ArraySize++;
+            }
+            else
+            {
+                T[] DkeyArr = KeyArray;
+                R[] DvalueArr = ValueArray;
+
+                KeyArray = new T[ArraySize];
+                ValueArray = new R[ArraySize];
+
+                for (int i = 0; i < DkeyArr.Length; i++)
+                {
+                    KeyArray[i] = DkeyArr[i];
+                    ValueArray[i] = DvalueArr[i];
+                }
+                KeyArray[ListLetgth] = key;
+                ValueArray[ListLetgth] = value;
+
+                listLetgth++;
+                ArraySize++;
+            }
+        }
+        public R GetElement(T value)
+        {
+            int index = Array.IndexOf(KeyArray, value);
+            return ValueArray[index];
+        }
+    }
+}
